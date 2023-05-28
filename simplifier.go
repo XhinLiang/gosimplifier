3e01c19f3150e74dd3d0d34689f6ca95efc04892
xhinliang <xhinliang@gmail.com>
2023-05-28 11:02:12 +0800

    Pass the UT.

--- simplifier.go
+++ simplifier.go
@@ -1,9 +1,15 @@
-package simplifier
+package gosimplifier
 
 import (
 	"encoding/json"
 	"reflect"
 )
+
+// Rule defines the rule structure for property removal and nested property rules.
+type Rule struct {
+	RemoveProperties    []string         `json:"remove_properties"`
+	PropertySimplifiers map[string]*Rule `json:"property_simplifiers"`
+}
 
 // Simplifier defines the interface for struct simplification.
 type Simplifier interface {
@@ -12,51 +18,56 @@ type Simplifier interface {
 
 // simplifierImpl implements the Simplifier interface.
 type simplifierImpl struct {
-	rules map[string]interface{}
+	propertySimplifiers map[string]Ruler
 }
+
+type Ruler interface {
+	applyRules(name interface{}, value reflect.Value)
+}
+
+type clearRuler struct {
+}
+
+var clearRulerIns = &clearRuler{}
 
 // NewSimplifier creates a new instance of simplifierImpl with the given rules based on the origin Simplifier.
 // If the origin is nil, a new Simplifier is created without any base rules.
-func NewSimplifier(origin Simplifier, rulesJSON string) (Simplifier, error) {
-	rules := make(map[string]interface{})
-
-	if origin != nil {
-		// Copy the rules from the origin Simplifier
-		rulesCopy, err := copyRules(origin)
-		if err != nil {
-			return nil, err
-		}
-
-		rules = rulesCopy
-	}
-
-	// Add or update the rules from the rulesJSON
-	err := json.Unmarshal([]byte(rulesJSON), &rules)
+func NewSimplifier(rulesJSON string) (Simplifier, error) {
+	rule := &Rule{}
+	err := json.Unmarshal([]byte(rulesJSON), rule)
 	if err != nil {
 		return nil, err
 	}
+	return newSimplifier0(rule)
+}
 
+func newSimplifier0(rule *Rule) (*simplifierImpl, error) {
+	propertySimplifiers, err := createPropertySimplifiers(rule)
+	if err != nil {
+		return nil, err
+	}
 	return &simplifierImpl{
-		rules: rules,
+		propertySimplifiers: propertySimplifiers,
 	}, nil
 }
 
-// copyRules creates a deep copy of the rules from the origin Simplifier.
-func copyRules(origin Simplifier) (map[string]interface{}, error) {
-	originalRules := reflect.ValueOf(origin).Elem().FieldByName("rules")
+// createPropertySimplifiers creates property simplifiers based on the provided rules.
+func createPropertySimplifiers(rule *Rule) (map[string]Ruler, error) {
+	propertySimplifiers := make(map[string]Ruler)
 
-	rulesBytes, err := json.Marshal(originalRules.Interface())
-	if err != nil {
-		return nil, err
+	for propName, subRule := range rule.PropertySimplifiers {
+		propertySimplifier, err := newSimplifier0(subRule)
+		if err != nil {
+			return nil, err
+		}
+		propertySimplifiers[propName] = propertySimplifier
 	}
 
-	rulesCopy := make(map[string]interface{})
-	err = json.Unmarshal(rulesBytes, &rulesCopy)
-	if err != nil {
-		return nil, err
+	for _, propName := range rule.RemoveProperties {
+		propertySimplifiers[propName] = clearRulerIns
 	}
 
-	return rulesCopy, nil
+	return propertySimplifiers, nil
 }
 
 // Simplify applies the rules to the original struct and returns a simplified copy.
@@ -69,45 +80,41 @@ func (s *simplifierImpl) Simplify(original interface{}) (interface{}, error) {
 	copy = deepCopy(copy, copyValue)
 
 	// Apply the rules recursively
-	s.applyRules(copy, s.rules)
+	s.applyRules("root", copy)
 
 	return copy.Interface(), nil
 }
 
-// deepCopy recursively copies the values from src to dst.
-func deepCopy(dst, src reflect.Value) reflect.Value {
-	if src.Kind() == reflect.Ptr {
-		if src.IsNil() {
-			return dst
+// deepCopy makes a deep copy of the original value recursively.
+func deepCopy(copy reflect.Value, original reflect.Value) reflect.Value {
+	switch original.Kind() {
+	case reflect.Struct:
+		for i := 0; i < original.NumField(); i++ {
+			copy.Field(i).Set(deepCopy(copy.Field(i), original.Field(i)))
 		}
-		dst.Set(reflect.New(src.Type().Elem()))
-		return deepCopy(dst.Elem(), src.Elem())
+	case reflect.Slice:
+		copy.Set(reflect.MakeSlice(original.Type(), original.Len(), original.Cap()))
+		for i := 0; i < original.Len(); i++ {
+			copy.Index(i).Set(deepCopy(copy.Index(i), original.Index(i)))
+		}
+	case reflect.Ptr:
+		copy.Set(reflect.New(original.Type().Elem()))
+		copy.Elem().Set(deepCopy(copy.Elem(), original.Elem()))
+	default:
+		copy.Set(original)
 	}
 
-	if src.Kind() == reflect.Struct {
-		for i := 0; i < src.NumField(); i++ {
-			srcField := src.Field(i)
-			dstField := dst.Field(i)
-			if srcField.CanInterface() {
-				dstField.Set(deepCopy(dstField, srcField))
-			}
-		}
-		return dst
-	}
-
-	if src.Kind() == reflect.Slice {
-		dst.Set(reflect.MakeSlice(src.Type(), src.Len(), src.Cap()))
-		for i := 0; i < src.Len(); i++ {
-			dst.Index(i).Set(deepCopy(dst.Index(i), src.Index(i)))
-		}
-		return dst
-	}
-
-	return src
+	return copy
 }
 
-// applyRules applies the given rules to the value recursively.
-func (s *simplifierImpl) applyRules(value reflect.Value, rules map[string]interface{}) {
+func (s *clearRuler) applyRules(_ interface{}, field reflect.Value) {
+	if field.IsValid() && field.CanSet() {
+		field.Set(reflect.Zero(field.Type()))
+	}
+}
+
+// applyRules applies the rules to the struct recursively.
+func (s *simplifierImpl) applyRules(name interface{}, value reflect.Value) {
 	if value.Kind() == reflect.Ptr {
 		if value.IsNil() {
 			return
@@ -115,26 +122,41 @@ func (s *simplifierImpl) applyRules(value reflect.Value, rules map[string]interf
 		value = value.Elem()
 	}
 
-	if value.Kind() == reflect.Struct {
-		for fieldName, fieldValue := range rules {
+	switch value.Kind() {
+	case reflect.Struct:
+		for fieldName, subSimplifier := range s.propertySimplifiers {
 			field := value.FieldByName(fieldName)
 			if !field.IsValid() || !field.CanInterface() {
 				continue
 			}
 
-			switch fieldValue := fieldValue.(type) {
-			case []interface{}:
-				if field.Kind() == reflect.Slice {
-					for i := 0; i < field.Len(); i++ {
-						item := field.Index(i)
-						if item.Kind() == reflect.Ptr {
-							item = item.Elem()
-						}
-						s.applyRules(item, fieldValue[i].(map[string]interface{}))
+			switch field.Kind() {
+			case reflect.Slice:
+				for i := 0; i < field.Len(); i++ {
+					item := field.Index(i)
+					if item.Kind() == reflect.Ptr {
+						item = item.Elem()
 					}
+					subSimplifier.applyRules(fieldName, item)
 				}
-			case map[string]interface{}:
-				s.applyRules(field, fieldValue)
+			default:
+				subSimplifier.applyRules(fieldName, field)
+			}
+		}
+	case reflect.Map:
+		for _, key := range value.MapKeys() {
+			subSimplifier := s.propertySimplifiers[key.String()]
+			if subSimplifier != nil {
+				subValue := value.MapIndex(key)
+				if subValue.Kind() == reflect.Ptr {
+					subValue = subValue.Elem()
+				}
+				if subSimplifier == clearRulerIns {
+					value.SetMapIndex(key, reflect.Value{})
+				} else {
+					subSimplifier.applyRules(key.Interface(), subValue)
+				}
+
 			}
 		}
 	}
--- simplifier_test.go
+++ simplifier_test.go
@@ -1,43 +1,74 @@
-package simplifier
+package gosimplifier
 
 import (
 	"reflect"
 	"testing"
 )
 
-// Example structs for testing
-type ExampleStruct struct {
-	Test       string
-	Debug      string
-	Data       []Data
-	EntityList []Entity
-}
-
-type Data struct {
-	DataTest  string
-	DataDebug string
-}
-
-type Entity struct {
-	SubProperties []SubProperties
-	EntityTest    string
-}
-
-type SubProperties struct {
-	ABC string
-	DEF string
-}
-
 func TestSimplify(t *testing.T) {
-	// Define the original struct for testing
-	original := ExampleStruct{
+	// Define an example struct
+	type SubProperties struct {
+		ABC string
+		DEF string
+	}
+
+	type Entity struct {
+		SubProperties []SubProperties
+		EntityTest    string
+	}
+
+	type ExampleStruct struct {
+		Test       string
+		Debug      bool
+		Data       map[string]interface{}
+		EntityList []Entity
+		OtherField string
+	}
+
+	// Define the rules JSON
+	rulesJSON := `
+{
+  "remove_properties": [
+    "Test",
+    "Debug"
+  ],
+  "property_simplifiers": {
+    "Data": {
+      "remove_properties": [
+        "data_test",
+        "data_debug"
+      ]
+    },
+    "EntityList": {
+      "remove_properties": [
+        "EntityTest"
+      ],
+      "property_simplifiers": {
+        "SubProperties": {
+          "remove_properties": [
+            "ABC",
+            "DEF"
+          ]
+        }
+      }
+    }
+  }
+}
+`
+
+	// Create a new Simplifier instance
+	simplifier, err := NewSimplifier(rulesJSON)
+	if err != nil {
+		t.Fatalf("Failed to create Simplifier: %v", err)
+	}
+
+	// Create an example struct
+	original := &ExampleStruct{
 		Test:  "test value",
-		Debug: "debug value",
-		Data: []Data{
-			{
-				DataTest:  "data test value",
-				DataDebug: "data debug value",
-			},
+		Debug: true,
+		Data: map[string]interface{}{
+			"data_test":  "data test value",
+			"data_debug": false,
 		},
 		EntityList: []Entity{
 			{
@@ -50,216 +81,24 @@ func TestSimplify(t *testing.T) {
 				EntityTest: "entity test value",
 			},
 		},
+		OtherField: "other value",
 	}
-
-	// Define the expected simplified struct based on the provided rules
-	expected := ExampleStruct{
-		Data: []Data{
-			{
-				DataDebug: "data debug value",
-			},
-		},
-		EntityList: []Entity{
-			{
-				SubProperties: []SubProperties{
-					{},
-				},
-			},
-		},
-	}
-
-	// Define the rules JSON
-	rulesJSON := `
-{
-  "remove_properties": [
-    "test",
-    "debug"
-  ],
-  "property_rules": {
-    "data": {
-      "remove_properties": [
-        "data_test",
-        "data_debug"
-      ],
-      "property_rules": {}
-    },
-    "entity_list": {
-      "remove_properties": [
-        "entity_test"
-      ],
-      "property_rules": {
-        "sub_properties": {
-          "remove_properties": [
-            "abc",
-            "def"
-          ]
-        }
-      }
-    }
-  }
-}
-`
-
-	// Create a new simplifier with the rules
-	simplifier, err := NewSimplifier(nil, rulesJSON)
-	if err != nil {
-		t.Fatalf("Error creating simplifier: %v", err)
-	}
-
 	// Simplify the original struct
 	simplified, err := simplifier.Simplify(original)
 	if err != nil {
-		t.Fatalf("Error simplifying struct: %v", err)
+		t.Fatalf("Failed to simplify struct: %v", err)
 	}
 
-	// Compare the simplified struct with the expected struct
+	// Validate the simplified struct
+	expected := &ExampleStruct{
+		Test:       "",
+		Debug:      false,
+		Data:       map[string]interface{}{},
+		EntityList: []Entity{{SubProperties: []SubProperties{{ABC: "", DEF: ""}}}},
+		OtherField: "other value",
+	}
+
 	if !reflect.DeepEqual(simplified, expected) {
-		t.Errorf("Simplified struct does not match expected struct.\nExpected: %+v\nActual: %+v", expected, simplified)
-	}
-}
-
-func TestNewSimplifierWithOrigin(t *testing.T) {
-	// Define the original rules for the origin Simplifier
-	originRulesJSON := `
-{
-  "property_rules": {
-    "data": {
-      "remove_properties": [
-        "data_test"
-      ],
-      "property_rules": {}
-    }
-  }
-}
-`
-
-	// Create the origin Simplifier
-	origin, err := NewSimplifier(nil, originRulesJSON)
-	if err != nil {
-		t.Fatalf("Error creating origin Simplifier: %v", err)
-	}
-
-	// Define the additional rules to be added to the origin Simplifier
-	additionalRulesJSON := `
-{
-  "remove_properties": [
-    "debug"
-  ],
-  "property_rules": {
-    "data": {
-      "remove_properties": [
-        "data_debug"
-      ],
-      "property_rules": {}
-    },
-    "entity_list": {
-      "remove_properties": [
-        "entity_test"
-      ],
-      "property_rules": {
-        "sub_properties": {
-          "remove_properties": [
-            "abc",
-            "def"
-          ]
-        }
-      }
-    }
-  }
-}
-`
-
-	// Create a new simplifier based on the origin Simplifier and additional rules
-	simplifier, err := NewSimplifier(origin, additionalRulesJSON)
-	if err != nil {
-		t.Fatalf("Error creating new Simplifier with origin: %v", err)
-	}
-
-	// Simplify an example struct using the new simplifier
-	original := ExampleStruct{
-		Test:       "test value",
-		Debug:      "debug value",
-		Data:       []Data{{DataTest: "data test value", DataDebug: "data debug value"}},
-		EntityList: []Entity{{EntityTest: "entity test value"}},
-	}
-
-	simplified, err := simplifier.Simplify(original)
-	if err != nil {
-		t.Fatalf("Error simplifying struct: %v", err)
-	}
-
-	// Define the expected simplified struct based on the origin rules and additional rules
-	expected := ExampleStruct{
-		Data:       []Data{{DataTest: "data test value"}},
-		EntityList: []Entity{{}},
-	}
-
-	// Compare the simplified struct with the expected struct
-	if !reflect.DeepEqual(simplified, expected) {
-		t.Errorf("Simplified struct does not match expected struct.\nExpected: %+v\nActual: %+v", expected, simplified)
-	}
-}
-
-func TestNewSimplifierWithoutOrigin(t *testing.T) {
-	// Define the rules for the new Simplifier
-	rulesJSON := `
-{
-  "remove_properties": [
-    "test",
-    "debug"
-  ],
-  "property_rules": {
-    "data": {
-      "remove_properties": [
-        "data_test",
-        "data_debug"
-      ],
-      "property_rules": {}
-    },
-    "entity_list": {
-      "remove_properties": [
-        "entity_test"
-      ],
-      "property_rules": {
-        "sub_properties": {
-          "remove_properties": [
-            "abc",
-            "def"
-          ]
-        }
-      }
-    }
-  }
-}
-`
-
-	// Create a new simplifier without an origin
-	simplifier, err := NewSimplifier(nil, rulesJSON)
-	if err != nil {
-		t.Fatalf("Error creating new Simplifier without origin: %v", err)
-	}
-
-	// Simplify an example struct using the new simplifier
-	original := ExampleStruct{
-		Test:       "test value",
-		Debug:      "debug value",
-		Data:       []Data{{DataTest: "data test value", DataDebug: "data debug value"}},
-		EntityList: []Entity{{EntityTest: "entity test value"}},
-	}
-
-	simplified, err := simplifier.Simplify(original)
-	if err != nil {
-		t.Fatalf("Error simplifying struct: %v", err)
-	}
-
-	// Define the expected simplified struct based on the rules
-	expected := ExampleStruct{
-		Data:       []Data{{DataDebug: "data debug value"}},
-		EntityList: []Entity{{SubProperties: []SubProperties{{}}}},
-	}
-
-	// Compare the simplified struct with the expected struct
-	if !reflect.DeepEqual(simplified, expected) {
-		t.Errorf("Simplified struct does not match expected struct.\nExpected: %+v\nActual: %+v", expected, simplified)
+		t.Errorf("Simplified struct does not match expected value.\nExpected: %+v\nActual: %+v", expected, simplified)
 	}
 }
